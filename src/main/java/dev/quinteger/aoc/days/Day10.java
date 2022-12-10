b389bfe72b882ea3aa9b44cd47fd8c1924819f24
Quinteger <counterdisclaimer@gmail.com>
2022-12-10 09:05:11 +0300

    Day 10 refactoring

--- src/main/java/dev/quinteger/aoc/days/Day10.java
+++ src/main/java/dev/quinteger/aoc/days/Day10.java
@@ -2,12 +2,16 @@ package dev.quinteger.aoc.days;
 
 import dev.quinteger.aoc.Solution;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Day10 extends Solution {
     public Day10(List<String> input) {
         super(input);
     }
+
+    private final Map<Integer, Integer> cycleToRegister = new HashMap<>();
 
     @Override
     public Object solvePart1() {
@@ -20,11 +24,13 @@ public class Day10 extends Solution {
             if (op.equals("noop")) {
                 cycle++;
                 sum += add(cycle, register);
+                cycleToRegister.put(cycle, register);
             } else if (op.equals("addx")) {
                 int toAdd = Integer.parseInt(split[1]);
                 for (int i = 0; i < 2; i++) {
                     cycle++;
                     sum += add(cycle, register);
+                    cycleToRegister.put(cycle, register);
                     if (i == 1) {
                         register += toAdd;
                     }
@@ -45,39 +51,16 @@ public class Day10 extends Solution {
 
     @Override
     public Object solvePart2() {
-        int cycle = 0;
-        int register = 1;
-        int sum = 0;
-        for (String line : input) {
-            var split = line.split(" ");
-            var op = split[0];
-            if (op.equals("noop")) {
-                cycle++;
-                sum += add(cycle, register);
-                draw(cycle, register);
-            } else if (op.equals("addx")) {
-                int toAdd = Integer.parseInt(split[1]);
-                for (int i = 0; i < 2; i++) {
-                    cycle++;
-                    sum += add(cycle, register);
-                    draw(cycle, register);
-                    if (i == 1) {
-                        register += toAdd;
-                    }
-                }
-            } else {
-                throw new RuntimeException();
-            }
-        }
-        return sum;
+        cycleToRegister.forEach(Day10::draw);
+        return "see above";
     }
 
     private static void draw(int cycle, int register) {
         int rowPosition = (cycle - 1) % 40;
         if (register >= rowPosition - 1 && register <= rowPosition + 1) {
-            System.out.print("#");
+            System.out.print("█");
         } else {
-            System.out.print(".");
+            System.out.print(" ");
         }
         if (cycle % 40 == 0) {
             System.out.println();
