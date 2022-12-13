c1bf7d6554fbe05a8682af6e130d0ca1abf5371e
Quinteger <counterdisclaimer@gmail.com>
2022-12-13 09:51:26 +0300

    Day 13 raw

---
+++ src/main/java/dev/quinteger/aoc/days/Day13.java
@@ -0,0 +1,188 @@
+package dev.quinteger.aoc.days;
+
+import dev.quinteger.aoc.Solution;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Day13 extends Solution {
+    public Day13(List<String> input) {
+        super(input);
+    }
+
+    private static class Packet implements Comparable<Packet> {
+        final List<Object> list = new ArrayList<>();
+
+        @SuppressWarnings("unchecked")
+        public void addElement(List<Integer> keys, Object element) {
+            List<Object> l = list;
+            for (int i = 0; i < keys.size() - 1; i++) {
+                int key = keys.get(i);
+                ensureCapacityForKey(l, key);
+                Object o = l.get(key);
+                if (o instanceof List<?>) {
+                    l = (List<Object>) o;
+                } else {
+                    throw new RuntimeException("Expected a list or null, but got " + o);
+                }
+            }
+            int key = keys.get(keys.size() - 1);
+            ensureCapacityForKey(l, key);
+            l.set(key, element);
+        }
+
+        private static void ensureCapacityForKey(List<Object> list, int key) {
+            while (key >= list.size()) {
+                list.add(new ArrayList<>());
+            }
+        }
+
+        @SuppressWarnings("unchecked")
+        public void ensureListExists(List<Integer> keys) {
+            List<Object> l = list;
+            for (int i = 0; i < keys.size() - 1; i++) {
+                int key = keys.get(i);
+                ensureCapacityForKey(l, key);
+                Object o = l.get(key);
+                if (o instanceof List<?>) {
+                    l = (List<Object>) o;
+                }
+            }
+        }
+
+        @Override
+        public int compareTo(Packet o) {
+            return comesBefore(this.list, o.list);
+        }
+
+        @SuppressWarnings("unchecked")
+        private static int comesBefore(Object o1, Object o2) {
+            if (o1 instanceof Integer i1 && o2 instanceof Integer i2) {
+                return i1.compareTo(i2);
+            } else if (o1 instanceof List<?> l1 && o2 instanceof List<?> l2) {
+                List<Object> list1 = (List<Object>) l1;
+                List<Object> list2 = (List<Object>) l2;
+                int endIndex = Math.max(list1.size(), list2.size());
+                for (int i = 0; i < endIndex; i++) {
+                    if (i >= list1.size()) {
+                        return -1;
+                    } else if (i >= list2.size()) {
+                        return 1;
+                    } else {
+                        var e1 = list1.get(i);
+                        var e2 = list2.get(i);
+                        int result = comesBefore(e1, e2);
+                        if (result != 0) {
+                            return result;
+                        }
+                    }
+                }
+                return 0;
+            } else if (o1 instanceof List<?> list) {
+                return comesBefore(list, Collections.singletonList(o2));
+            } else if (o2 instanceof List<?> list) {
+                return comesBefore(Collections.singletonList(o1), list);
+            }
+            throw new RuntimeException("Comparing %s and %s".formatted(o1, o2));
+        }
+
+        @Override
+        public String toString() {
+            return list.toString();
+        }
+    }
+
+    @Override
+    public Object solvePart1() {
+        int pairIndex = 0;
+        int sum = 0;
+        for (int lineIndex = 0; lineIndex < input.size(); lineIndex+=3) {
+            var packet1 = createPacket(input.get(lineIndex));
+            var packet2 = createPacket(input.get(lineIndex + 1));
+            pairIndex++;
+            System.out.println(packet1);
+            System.out.println(packet2);
+            System.out.println(packet1.compareTo(packet2));
+            int result = packet1.compareTo(packet2);
+            if (result < 0) {
+                sum += pairIndex;
+            }
+        }
+        return sum;
+    }
+
+    private static Packet createPacket(String line) {
+        Packet packet = new Packet();
+        Map<Integer, Integer> groups = new TreeMap<>();
+        int currentDepth = 0;
+        int currentGroup = 0;
+        var chars = line.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            char c = chars[i];
+            if (c == ',') {
+                currentGroup++;
+                groups.put(currentDepth, currentGroup);
+            } else if (c == '[') {
+                groups.put(currentDepth, currentGroup);
+                currentDepth++;
+                currentGroup = 0;
+                groups.put(currentDepth, currentGroup);
+            } else if (c == ']') {
+                currentDepth--;
+                currentGroup = groups.get(currentDepth);
+                packet.ensureListExists(groups.values().stream().toList());
+            } else if (Character.isDigit(c)) {
+                int numberStart = i;
+                c = chars[i + 1];
+                while (Character.isDigit(c) && i < chars.length) {
+                    i++;
+                    c = chars[i + 1];
+                }
+                int numberEnd = i + 1;
+                int number = Integer.parseInt(line.substring(numberStart, numberEnd));
+                List<Integer> groupValues = groups.values().stream().toList();
+                packet.addElement(groupValues, number);
+            }
+        }
+        return packet;
+    }
+
+    @Override
+    public Object solvePart2() {
+        List<Packet> packets = new ArrayList<>();
+        for (int lineIndex = 0; lineIndex < input.size(); lineIndex++) {
+            var line = input.get(lineIndex);
+            if (!line.isEmpty()) {
+                var packet = createPacket(input.get(lineIndex));
+                packets.add(packet);
+            }
+        }
+        packets.add(createPacket("[[2]]"));
+        packets.add(createPacket("[[6]]"));
+        packets.sort(Packet::compareTo);
+        try (var writer = Files.newBufferedWriter(Path.of("result.txt"), StandardOpenOption.CREATE)) {
+            for (int i = 0; i < packets.size(); i++) {
+                Packet packet = packets.get(i);
+                writer.write(packet.toString());
+                writer.newLine();
+                if (packet.list.equals(Collections.singletonList(Collections.singletonList(Collections.singletonList(2))))) {
+                    System.out.println(i + 1);
+                }
+                if (packet.list.equals(Collections.singletonList(Collections.singletonList(Collections.singletonList(6))))) {
+                    System.out.println(i + 1);
+                }
+            }
+            writer.flush();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return null;
+    }
+}
