a3cf63fa6964b812cea1d2122fb716780d8b9a30
Quinteger <counterdisclaimer@gmail.com>
2022-12-14 11:26:57 +0300

    Day 14 refactoring

--- src/main/java/dev/quinteger/aoc/days/Day14.java
+++ src/main/java/dev/quinteger/aoc/days/Day14.java
@@ -9,7 +9,8 @@ public class Day14 extends Solution {
     public Day14(List<String> input) {
         super(input);
     }
-    private static final int origin = 500;
+    private static final int originOffset = 500;
+    private static final Point origin = new Point(0, originOffset);
 
     private char[][] grid = new char[1][1];
     private int distance = 0;
@@ -20,9 +21,6 @@ public class Day14 extends Solution {
         if (distanceDiff <= 0) {
             return;
         }
-//        if (distanceDiff % 2 !=0) {
-//            throw new RuntimeException();
-//        }
         int newHeight = distance + 1;
         int newWidth = distance * 2 + 1;
         char[][] newGrid = new char[newHeight][newWidth];
@@ -57,26 +55,26 @@ public class Day14 extends Solution {
                 int endCol = Integer.parseInt(endPoint[0]);
                 int endRow = Integer.parseInt(endPoint[1]);
 
-                resizeGridForDistance(Math.max(Math.max(Math.abs(startCol - origin), Math.abs(endCol - origin)) + 1, Math.max(startRow, endRow) + 2));
+                resizeGridForDistance(Math.max(Math.max(Math.abs(startCol - originOffset), Math.abs(endCol - originOffset)) + 1, Math.max(startRow, endRow) + 2));
 
                 if (startCol == endCol) {
                     int min = Math.min(startRow, endRow);
                     int max = Math.max(startRow, endRow);
                     for (int j = min; j <= max; j++) {
-                        grid[j][startCol - origin + distance] = '#';
+                        grid[j][startCol - originOffset + distance] = '#';
                     }
                 } else if (startRow == endRow) {
                     int min = Math.min(startCol, endCol);
                     int max = Math.max(startCol, endCol);
                     for (int j = min; j <= max; j++) {
-                        grid[startRow][j - origin + distance] = '#';
+                        grid[startRow][j - originOffset + distance] = '#';
                     }
                 } else {
                     throw new RuntimeException();
                 }
             }
         }
-        printGrid();
+//        printGrid();
     }
 
     private void printGrid() {
@@ -93,75 +91,71 @@ public class Day14 extends Solution {
     public Object solvePart1() {
         fillGrid();
 
-        var sandPrev = spawnSand();
-        var sandNext = spawnSand();
-        int count = 2;
+        Point newPoint;
+        int count = 0;
         do {
-            sandPrev = sandNext;
-            sandNext = spawnSand();
-            count++;
+            newPoint = spawnSand();
+            if (newPoint != null) {
+                count++;
+            }
+            if (distance <= 10) {
+                printGrid();
+            }
 
-//            printGrid();
+        } while (newPoint != null && !origin.equals(newPoint));
 
-        } while (sandNext != null && !sandNext.equals(sandPrev));
-
-//        printGrid();
-
-        return count - 1;
+        return count;
     }
 
     private Point spawnSand() {
-        var sandPrev = new Point(0, 500);
-        var sandNext = sandPrev.tryGoDown(grid, distance);
+        var sandPrev = new Point(0, originOffset);
+        var sandNext = tryGoDown(sandPrev);
+
         while (!sandPrev.equals(sandNext) && sandNext != null) {
             sandPrev = sandNext;
-            sandNext = sandNext.tryGoDown(grid, distance);
+            sandNext = tryGoDown(sandNext);
         }
+
         if (sandNext != null) {
-            grid[sandNext.x()][sandNext.y() - origin + distance] = '0';
+            grid[sandNext.x()][sandNext.y() - originOffset + distance] = '0';
         }
         return sandNext;
+    }
+
+    public Point tryGoDown(Point sand) {
+        int realRow = sand.x();
+        int realCol = sand.y() - originOffset + distance;
+        if (realRow >= grid.length - 1) {
+            return null;
+        } else if (grid[realRow + 1][realCol] == '.') {
+            return new Point(sand.x() + 1, sand.y());
+        } else if (grid[realRow + 1][realCol - 1] == '.') {
+            return new Point(sand.x() + 1, sand.y() - 1);
+        } else if (grid[realRow + 1][realCol + 1] == '.') {
+            return new Point(sand.x() + 1, sand.y() + 1);
+        } else {
+            return sand;
+        }
     }
 
     @Override
     public Object solvePart2() {
         fillGrid();
 
-        char[] bottom = grid[grid.length - 1];
-        Arrays.fill(bottom, '#');
+        Arrays.fill(grid[grid.length - 1], '#');
 
-//        printGrid();
-
-        var start = new Point(0, 500);
         int count = 0;
         Point newPoint = null;
-        while (!start.equals(newPoint)) {
+        while (!origin.equals(newPoint)) {
             newPoint = spawnSand();
             count++;
-//            if (count % 10 == 0) {
-//                printGrid();
-//            }
-//            printGrid();
+            if (distance <= 10) {
+                printGrid();
+            }
         }
 
         return count;
     }
 
-    private record Point(int x, int y) {
-        public Point tryGoDown(char[][] grid, int distance) {
-            int realRow = x;
-            int realCol = y - origin + distance;
-            if (realRow >= grid.length - 1) {
-                return null;
-            } else if (grid[realRow + 1][realCol] == '.') {
-                return new Point(x + 1, y);
-            } else if (grid[realRow + 1][realCol - 1] == '.') {
-                return new Point(x + 1, y - 1);
-            } else if (grid[realRow + 1][realCol + 1] == '.') {
-                return new Point(x + 1, y + 1);
-            } else {
-                return this;
-            }
-        }
-    }
+    private record Point(int x, int y) { }
 }
